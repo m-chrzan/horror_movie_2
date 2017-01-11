ecbd6142e6d695c5979299160649c42f210e9bea
Bartosz Burny <bb370739@students.mimuw.edu.pl>
2017-01-11 11:44:51 +0100

    citizen & helper

--- citizen.h
+++ citizen.h
@@ -1,4 +1,39 @@
 #ifndef CITIZEN_H
 #define CITIZEN_H
 
+#include "helper.h"
+
+class Citizen : public Character {
+protected:
+	Age age;
+	
+	Citizen(HealthPoints hp, Age age) : Character(hp), age(age) {
+		assert(age > 0);
+	}
+	
+public:
+	Age getAge() {
+		return age;
+	}
+};
+
+class Teen : public Citizen {
+public:
+	Teen(HealthPoints hp, Age age) : Citizen(hp, age) {
+		assert(age < 17);
+	}
+};
+
+class Adult : public Citizen {
+public:
+	Adult(HealthPoints hp, Age age) : Citizen(hp, age) {
+		assert(age > 18);
+	}
+};
+
+class Sheriff : public Adult, public Attacker {
+public:
+	Sheriff(HealthPoints hp, Age age, AttackPower ap) : Adult(hp, age), Attacker(ap) {}
+};
+
 #endif
--- helper.h
+++ helper.h
@@ -5,4 +5,35 @@ using HealthPoints = double;
 using Age = double;
 using AttackPower = double;
 
+class Character {
+protected:
+	HealthPoints hp;
+	
+	Character(HealthPoints hp) : hp(hp) {
+		assert(hp > 0);
+	}
+	
+public:
+	HealthPoints getHealth() {
+		return hp;
+	}
+	
+	virtual void takeDamage(AttackPower damage) {
+		hp = max(hp - damage, 0.0);
+	}
+};
+
+class Attacker {
+private:
+	AttackPower ap;
+	
+protected:
+	Attacker(AttackPower ap) : ap(ap) {}
+	
+public:
+	AttackPower getAttackPower() {
+		return ap;
+	}
+};
+
 #endif
