7da4e191117deca06a23f5ad084d90d6fe2a7d13
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-01-11 11:14:39 +0100

    Add testing files

---
+++ testing.h
@@ -0,0 +1,69 @@
+#ifndef TESTING_H_
+#define TESTING_H_
+
+#include <functional>
+#include <iostream>
+#include <set>
+#include <string>
+
+#define beginTest() std::cout << "Starting " << __FUNCTION__ << ".\n"
+
+void check(bool test, std::string message) {
+    if (test) {
+        std::cout << "  Test ok. " << message << std::endl;
+    } else {
+        std::cout << "  TEST FAILED! " << message << std::endl;
+    }
+}
+
+void checkFalse(bool test, std::string message) {
+    check(!test, message);
+}
+
+template <class T>
+void checkEqual(T const& p1, T const& p2, std::string message) {
+    check(p1 == p2, message);
+}
+
+template <class T>
+void checkNotEqual(T const& p1, T const& p2, std::string message) {
+    checkFalse(p1 == p2, message);
+}
+
+template <class T>
+void checkSameSet(T const& p1, T const& p2, std::string message) {
+    std::set<typename T::value_type> set1(p1.begin(), p1.end());
+    std::set<typename T::value_type> set2(p2.begin(), p2.end());
+
+    check(set1 == set2, message);
+}
+
+void checkNoExceptionThrown(std::function<void(void)> function,
+        std::string message) {
+    bool thrown = false;
+
+    try {
+        function();
+    } catch (...) {
+        thrown = true;
+    }
+
+    checkFalse(thrown, message);
+}
+
+template <class E>
+void checkExceptionThrown(std::function<void(void)> function, std::string message) {
+    bool thrown = false;
+
+    try {
+        function();
+    } catch (E e) {
+        thrown = true;
+    } catch (...) {
+        thrown = false;
+    }
+
+    check(thrown, message);
+}
+
+#endif
