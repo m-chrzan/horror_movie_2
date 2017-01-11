1eb637007c5f56532376870df1df474ec9743576
Bartek <bb370739@students.mimuw.edu.pl>
2017-01-11 18:39:35 +0100

    minor fixes

--- citizen.h
+++ citizen.h
@@ -6,39 +6,39 @@
 
 class Citizen : public Character {
 private:
-	Age age_;
+    Age age_;
 
 public:
-	Citizen(HealthPoints health, Age age) : Character(health), age_(age) {}
+    Citizen(HealthPoints health, Age age) : Character(health), age_(age) {}
 
     virtual ~Citizen() = 0;
 
-	Age getAge() {
-		return age_;
-	}
+    Age getAge() {
+        return age_;
+    }
 };
 
 inline Citizen::~Citizen() {}
 
 class Teenager : public Citizen {
 public:
-	Teenager(HealthPoints health, Age age) : Citizen(health, age) {
-		assert(age <= 17);
-		assert(age >= 11);
-	}
+    Teenager(HealthPoints health, Age age) : Citizen(health, age) {
+        assert(age <= 17);
+        assert(age >= 11);
+    }
 };
 
 class Adult : public Citizen {
 public:
-	Adult(HealthPoints health, Age age) : Citizen(health, age) {
-		assert(age >= 18);
-		assert(age <= 100);
-	}
+    Adult(HealthPoints health, Age age) : Citizen(health, age) {
+        assert(age >= 18);
+        assert(age <= 100);
+    }
 };
 
 class Sheriff : public Adult, public Attacker {
 public:
-	Sheriff(HealthPoints health, Age age, AttackPower power) :
+    Sheriff(HealthPoints health, Age age, AttackPower power) :
             Adult(health, age), Attacker(power) {}
 };
 
--- helper.h
+++ helper.h
@@ -2,49 +2,48 @@
 #define HELPER_H
 
 #include <cassert>
-#include <algorithm>
 
 using HealthPoints = double;
 using Age = double;
 using AttackPower = double;
 
 class Character {
-private:
-	HealthPoints health_;
+protected:
+    HealthPoints health_;
 
 public:
-	Character(HealthPoints health) : health_(health) {
-		assert(health_ > 0);
-	}
+    Character(HealthPoints health) : health_(health) {
+        assert(health_ > 0);
+    }
 
     virtual ~Character() = 0;
 
-	HealthPoints getHealth() {
-		return health_;
-	}
+    HealthPoints getHealth() {
+        return health_;
+    }
 
-	virtual void takeDamage(AttackPower damage) {
+    virtual void takeDamage(AttackPower damage) {
         if (damage > health_) {
             health_ = 0;
         } else {
             health_ -= damage;
         }
-	}
+    }
 };
 
 inline Character::~Character() {}
 
 class Attacker {
-private:
-	AttackPower power_;
+protected:
+    AttackPower power_;
 
 public:
-	Attacker(AttackPower power) : power_(power) {}
+    Attacker(AttackPower power) : power_(power) {}
 
     virtual ~Attacker() = 0;
-	AttackPower getAttackPower() {
-		return power_;
-	}
+    AttackPower getAttackPower() {
+        return power_;
+    }
 };
 
 inline Attacker::~Attacker() {}
