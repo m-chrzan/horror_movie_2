5cd7c27ce12a2f64f90e4cdebe38e0ddde117a4c
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-01-11 19:47:15 +0100

    Test Citizen takeDamage

--- citizen_test.cc
+++ citizen_test.cc
@@ -39,8 +39,70 @@ void testSheriffConstructor() {
     checkEqual(sheriff2.getAttackPower(), 42.0, "Attack power set correctly.");
 }
 
+void testTeenagerTakeDamage() {
+    beginTest();
+
+    Teenager teenager1(20.0, 11.0);
+
+    teenager1.takeDamage(1.0);
+    checkEqual(teenager1.getHealth(), 19.0, "Took damage.");
+
+    teenager1.takeDamage(19.0);
+    checkEqual(teenager1.getHealth(), 0.0, "Took damage.");
+
+    teenager1.takeDamage(42.0);
+    checkEqual(teenager1.getHealth(), 0.0, "No damage taken when at 0.");
+
+    Teenager teenager2(100.0, 13.0);
+    teenager2.takeDamage(989123.0);
+    checkEqual(teenager2.getHealth(), 0.0, "Damage doesn't go below 0.");
+}
+
+void testAdultTakeDamage() {
+    beginTest();
+
+    Adult adult1(20.0, 21.0);
+
+    adult1.takeDamage(1.0);
+    checkEqual(adult1.getHealth(), 19.0, "Took damage.");
+
+    adult1.takeDamage(19.0);
+    checkEqual(adult1.getHealth(), 0.0, "Took damage.");
+
+    adult1.takeDamage(42.0);
+    checkEqual(adult1.getHealth(), 0.0, "No damage taken when at 0.");
+
+    Adult adult2(100.0, 99.0);
+    adult2.takeDamage(989123.0);
+    checkEqual(adult2.getHealth(), 0.0, "Damage doesn't go below 0.");
+}
+
+void testSheriffTakeDamage() {
+    beginTest();
+
+    Sheriff sheriff1(20.0, 21.0, 1.0);
+
+    sheriff1.takeDamage(1.0);
+    checkEqual(sheriff1.getHealth(), 19.0, "Took damage.");
+    checkEqual(sheriff1.getAttackPower(), 1.0, "Attack power unchanged.");
+
+    sheriff1.takeDamage(19.0);
+    checkEqual(sheriff1.getHealth(), 0.0, "Took damage.");
+
+    sheriff1.takeDamage(42.0);
+    checkEqual(sheriff1.getHealth(), 0.0, "No damage taken when at 0.");
+
+    Sheriff sheriff2(100.0, 99.0, 20000.0);
+    sheriff2.takeDamage(989123.0);
+    checkEqual(sheriff2.getHealth(), 0.0, "Damage doesn't go below 0.");
+}
+
 int main() {
     testTeenagerConstructor();
     testAdultConstructor();
     testSheriffConstructor();
+
+    testTeenagerTakeDamage();
+    testAdultTakeDamage();
+    testSheriffTakeDamage();
 }
