bd79ad951c3a0c3e14558b6afb8a6a984e7deac2
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-01-12 11:09:48 +0100

    Implement SmallTownBuilder

--- smalltown.h
+++ smalltown.h
@@ -1,6 +1,9 @@
 #ifndef SMALLTOWN_H
 #define SMALLTOWN_H
 
+#include <set>
+#include <vector>
+
 #include "citizen.h"
 #include "monster.h"
 
@@ -22,10 +25,12 @@ public:
     }
 };
 
+class SmallTownBuilder;
+
 class SmallTown {
 private:
-    Time current_time_;
-    Time max_time_;
+    Time currentTime_;
+    Time maxTime_;
 
     std::shared_ptr<Monster> monster_;
     std::vector<std::shared_ptr<Citizen>> citizens_;
@@ -33,15 +38,77 @@ private:
     std::shared_ptr<Strategy> strategy_;
 
 public:
-    SmallTown(Time start_time, Time max_time, std::shared_ptr<Monster> monster,
+    using Builder = SmallTownBuilder;
+    SmallTown(Time startTime, Time maxTime, std::shared_ptr<Monster> monster,
               std::vector<std::shared_ptr<Citizen>> citizens,
               std::shared_ptr<Strategy> strategy) :
-        current_time_(start_time), max_time_(max_time), monster_(monster),
+        currentTime_(startTime), maxTime_(maxTime), monster_(monster),
         citizens_(citizens), strategy_(strategy) {}
+};
 
-    SmallTown(Time start_time, Time max_time, std::shared_ptr<Monster> monster,
-              std::vector<std::shared_ptr<Citizen>> citizens) :
-        SmallTown(start_time, max_time, monster, citizens, std::make_shared<DefaultStrategy>()) {}
+class SmallTownBuilder {
+private:
+    std::vector<std::shared_ptr<Citizen>> citizens_;
+    std::set<std::shared_ptr<Citizen>> citizensSet_;
+    std::shared_ptr<Monster> monster_;
+    Time startTime_;
+    Time maxTime_;
+    std::shared_ptr<Strategy> strategy_;
+
+    void checkReadyToBuild_() {
+        if (monster_.get() == nullptr) {
+            throw std::logic_error("SmallTown needs a monster.");
+        }
+
+        if (citizensSet_.empty()) {
+            throw std::logic_error("SmallTown needs at least one citizen.");
+        }
+
+        if (startTime_ > maxTime_) {
+            throw std::logic_error("Start time can't be larger than max time.");
+        }
+    }
+
+public:
+    SmallTownBuilder() : strategy_(std::make_shared<DefaultStrategy>()) {}
+    SmallTownBuilder & citizen(std::shared_ptr<Citizen> citizen) {
+        if (citizensSet_.find(citizen) != citizensSet_.end()) {
+            citizensSet_.insert(citizen);
+            citizens_.push_back(citizen);
+        }
+
+        return *this;
+    }
+
+    SmallTownBuilder & monster(std::shared_ptr<Monster> monster) {
+        monster_ = monster;
+
+        return *this;
+    }
+
+    SmallTownBuilder & strategy(std::shared_ptr<Strategy> strategy) {
+        strategy_ = strategy;
+
+        return *this;
+    }
+
+    SmallTownBuilder & startTime(Time startTime) {
+        startTime_ = startTime;
+
+        return *this;
+    }
+
+    SmallTownBuilder & maxTime(Time maxTime) {
+        maxTime_ = maxTime;
+
+        return *this;
+    }
+
+    SmallTown build() {
+        checkReadyToBuild_();
+
+        return SmallTown(startTime_, maxTime_, monster_, citizens_, strategy_);
+    }
 };
 
 #endif
